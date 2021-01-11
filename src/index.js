ee92a1f7ef83938fd0a67bb3c475389da46f9b75
Mickie4 <findmickie4@gmail.com>
2021-01-11 03:28:37 +0000

    added a couple of functions to JS

--- src/index.js
+++ src/index.js
@@ -1,73 +1,58 @@
 
-let weather = {
-  "paris": {
-    temp: 19.7,
-    humidity: 80
-  },
-  "tokyo": {
-    temp: 17.3,
-    humidity: 50
-  },
-  "lisbon": {
-    temp: 30.2,
-    humidity: 20
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  "moscow": {
-    temp: -5,
-    humidity: 20
-  }
-};
+//Current time function
 
-let city = prompt(`Enter a city please? 💫`);
-city = city.toLowerCase();
-city = city.trim();
 
-if (weather[city] !== undefined) {
+let currentTime = new Date();
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
 
-let temperature = weather[city].temp;
-let celsiusTemp = Math.round(temperature);
-let fahrenheitTemp =Math.round((temperature * 9/5) + 32);
-let humidity = weather[city].humidity;
-alert(`It is currently ${celsiusTemp}°C (${fahrenheitTemp}°F) in ${city} with a humidity of ${humidity}%`);
+let formatTime = `${hours}:${minutes}`;
 
-} else {
-    alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`);
+let htmlTime = document.querySelector("#current-time");
+
+htmlTime.innerHTML = formatTime;
+
+//Search Engine 
+
+function searchFunction(event) {
+    event.preventDefault();
+    let cityInput = document.querySelector("#search-name");
+    let cityName = cityInput.value;
+    let cityH2 = document.querySelector("#city-name");
+
+    cityH2.innerHTML = `${cityName}`;
 }
 
+let searchForm = document.querySelector("#search-form");
 
+searchForm.addEventListener("submit", searchFunction);
 
+//Change the temperature
 
-
-
-
-/*function cityWeather() {
-    let city = prompt("Enter a city please?");
-    city = city.toLowerCase();
-    city = city.trim();
-if (city === "paris") {
-    alert(`It is currently ${weather.paris.temp}C in ${city} with a humidity of ${weather.paris.humidity}%`);
-} if (city === "tokyo") {
-    alert(`It is currently ${weather.tokyo.temp}C in ${city} with a humidity of ${weather.tokyo.humidity}%`);
-} if (city === "lisbon") {
-    alert(`It is currently ${weather.lisbon.temp}C in ${city} with a humidity of ${weather.lisbon.humidity}%`);
-} if (city === "san francisco") {
-    alert(`It is currently ${weather.sanFrancisco.temp}C in ${city} with a humidity of ${weather.sanFrancisco.humidity}%`);
-} if (city === "moscow") {
-    alert(`It is currently ${weather.moscow.temp}C in ${city} with a humidity of ${weather.moscow.humidity}%`);
-}  else {
-    alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`);
-}
-
+function TempInF(event) {
+    event.preventDefault();
+    let currentTemp = 9;
+    let conversionToF = (currentTemp * 9/5) + 32;
+    let tempDisplay = document.querySelector("#temp-display");
+    tempDisplay.innerHTML = `${conversionToF}°F`;
 
 }
 
-cityWeather(weather);
+let farButton = document.querySelector("#temp-f");
+farButton.addEventListener("click", TempInF);
 
-*/
+//Change back to Celsius
+
+function changeBack(event) {
+    event.preventDefault();
+    let orginalTemp = 9;
+    let tempDisplay = document.querySelector("#temp-display");
+    tempDisplay.innerHTML = `${orginalTemp}°C`
+}
+
+let celButton = document.querySelector("#temp-c");
+celButton.addEventListener("click", changeBack);
+
 
 
 
