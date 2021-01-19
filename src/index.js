062b1872a18ce5247a59530eef48b3df25af0d16
Mickie4 <40367446+Mickie4@users.noreply.github.com>
2021-01-19 01:59:49 +0000

    Started to work on the Search Engine and the API integration
    on this commit I am saving the API integration, I have created the functions for the search engine and the displays with vanilla JS.

--- src/index.js
+++ src/index.js
@@ -1,140 +1,38 @@
-//Current time function
+function searchEngine(event) {
+    event.preventDefault()
 
-let currentTime = new Date();
-let hours = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    function showInfo(response) {
+        let cityName = response.data.name;
+        let countryName = response.data.sys.country;
+        let cityTemp = Math.round(response.data.main.temp);
+        let description = response.data.weather[0].description;
+        let humidity = response.data.main.humidity;
+        let speed = response.data.wind.speed;
 
-day = days[currentTime.getDay()];
+        let cityDisplay = document.querySelector("#city-name");
+        cityDisplay.innerHTML = `${cityName}, ${countryName}`;
+        let tempDisplay = document.querySelector("#temp");
+        tempDisplay.innerHTML = `${cityTemp}°C`;
+        let descriptionDisplay = document.querySelector("#description");
+        descriptionDisplay.innerHTML = `${description}`;
+        let humidityDisplay = document.querySelector("#humidity");
+        humidityDisplay.innerHTML = `${humidity}%`;
+        let speedDisplay = document.querySelector("#speed");
+        speedDisplay.innerHTML = `${speed} Km/h`
 
-let formatTime = `${hours}:${minutes}`;
-
-let htmlTime = document.querySelector("#current-time");
-
-htmlTime.innerHTML = `Last Updated: ${day} ${formatTime}`;
-
-//Search Engine 
-
-function searchFunction(event) {
-    event.preventDefault();
-    let cityInput = document.querySelector("#search-name");
-    let cityName = cityInput.value;
-    let cityH2 = document.querySelector("#city-name");
-
-    cityH2.innerHTML = `${cityName}`;
-
-    //City Temperature 
-
-    function cityTempInfo(response) {
-        let tempResult = Math.round(response.data.main.temp);
-        let tempDisplay = document.querySelector("#temp-display");
-        tempDisplay.innerHTML = `${tempResult}°C`
 
     }
 
-
-    //API to search for cityInput temperature in Celsius
-
-    let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather?q=";
+    let cityInput = document.querySelector("#search-city");
+    let city = `${cityInput.value}`;
     let apiKey = "0603e85b4ce086e6bb52d7cdc7bcffb5";
-    let celsius = "metric"
-    let fahrenheit = "imperial"
-    let apiUrl = `${apiEndPoint}${cityName}&units=${celsius}&appid=${apiKey}`
+    let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather?q=";
+    let cel = "metric";
+    let far = "imperial";
+    let apiUrl = `${apiEndPoint}${city}&units=${cel}&appid=${apiKey}`;
 
-    axios.get(apiUrl).then(cityTempInfo);
+    axios.get(apiUrl).then(showInfo);
 }
 
-//Search engine function ends here!
-
-let searchForm = document.querySelector("#search-form");
-
-searchForm.addEventListener("submit", searchFunction);
-
-
-
-//Geolocation button for current temp
-function findCurrentLocation(event) {
-    event.preventDefault();
-
-    function currentTemperature(response) {
-        let temperature = Math.round(response.data.main.temp);
-        let currentCity = response.data.name;
-        let displayCity = document.querySelector("#city-name");
-        displayCity.innerHTML = `${currentCity}`
-        let displayTemp = document.querySelector("#temp-display");
-        displayTemp.innerHTML = `${temperature}°C`
-        console.log(currentCity);
-
-    }
-
-    function positionData(position) {
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
-        let units = "metric";
-        let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather?"
-        let apiKey = "0603e85b4ce086e6bb52d7cdc7bcffb5"
-        let apiUrl = `${apiEndPoint}lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
-        axios.get(apiUrl).then(currentTemperature);
-    }
-
-    navigator.geolocation.getCurrentPosition(positionData);
-}
-
-
-let locationBtn = document.querySelector("#current-location");
-locationBtn.addEventListener("click", findCurrentLocation);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Change the temperature
-
-function TempInF(event) {
-    event.preventDefault();
-    let currentTemp = 9;
-    let conversionToF = (currentTemp * 9 / 5) + 32;
-    let tempDisplay = document.querySelector("#temp-display");
-    tempDisplay.innerHTML = `${conversionToF}°F`;
-
-}
-
-let farButton = document.querySelector("#temp-f");
-farButton.addEventListener("click", TempInF);
-
-//Change back to Celsius
-
-function changeBack(event) {
-    event.preventDefault();
-    let orginalTemp = 9;
-    let tempDisplay = document.querySelector("#temp-display");
-    tempDisplay.innerHTML = `${orginalTemp}°C`
-}
-
-let celButton = document.querySelector("#temp-c");
-celButton.addEventListener("click", changeBack);
+let searchBtn = document.querySelector("#search-form");
+searchBtn.addEventListener("submit", searchEngine);
